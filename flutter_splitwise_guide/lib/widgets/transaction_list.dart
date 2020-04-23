a17bd0366b39e719152952045457c0dba8671f3a
Dipeshkawadkar <33801752+Dipeshkawadkar@users.noreply.github.com>
2020-04-23 09:28:33 +0530

    firstCommit

---
+++ flutter_splitwise_guide/lib/widgets/transaction_list.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import '../widgets/transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                  key: ValueKey(tx.id),
+                      transaction: tx,
+                      deleteTx: deleteTx,
+                    ))
+                .toList());
+  }
+}
+
+// Card(
+//                   child: Row(
+//                     children: <Widget>[
+//                       Container(
+//                         margin: EdgeInsets.symmetric(
+//                           vertical: 10,
+//                           horizontal: 15,
+//                         ),
+//                         decoration: BoxDecoration(
+//                           border: Border.all(
+//                             color: Theme.of(context).primaryColor,
+//                             width: 2,
+//                           ),
+//                         ),
+//                         padding: EdgeInsets.all(10),
+//                         child: Text(
+//                           '\$${transactions[index].amount.toStringAsFixed(2)}',
+//                           style: TextStyle(
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.bold,
+//                             color: Theme.of(context).primaryColor,
+//                           ),
+//                         ),
+//                       ),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Text(
+//                             transactions[index].title,
+//                             style: Theme.of(context).textTheme.title,
+//                           ),
+//                           Text(
+//                             DateFormat.yMMMEd()
+//                                 .format(transactions[index].date),
+//                             style: TextStyle(
+//                               color: Colors.grey,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 );
+//               },
